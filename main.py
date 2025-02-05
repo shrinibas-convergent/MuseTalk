c42567e91049f43ec83e656619e44f49c57c83a0
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2025-02-05 16:21:25 +0530

    stops reloader

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import uvicorn
 from ws_api import app
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
